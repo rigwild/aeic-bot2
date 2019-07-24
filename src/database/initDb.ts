896b3f9656e55393a7b9637aa0167c499bc95d6b
rigwild <contact@asauvage.fr>
2019-07-25 00:37:24 +0200

    Added planning reminder

--- src/database/initDb.ts
+++ src/database/initDb.ts
@@ -2,19 +2,21 @@ import { TpGroupModel, TpGroup } from './TpGroup'
 import { logger } from '../config'
 
 export const defaultTpGroups: TpGroup[] = [
-  ['tp1a', 'dut1', 'td1'],
-  ['tp1b', 'dut1', 'td1'],
-  ['tp1c', 'dut1', 'td2'],
-  ['tp1d', 'dut1', 'td2'],
-  ['tp1e', 'dut1', 'td3'],
-  ['tp2a', 'dut2', 'td1'],
-  ['tp2b', 'dut2', 'td1'],
-  ['tp2c', 'dut2', 'td2'],
-  ['tp2d', 'dut2', 'td2'],
+  ['tp1a', 'dut1', 'td1', 'DUT1 TPA', 'tp1a-remind'],
+  ['tp1b', 'dut1', 'td1', 'DUT1 TPB', 'tp1b-remind'],
+  ['tp1c', 'dut1', 'td2', 'DUT1 TPC', 'tp1c-remind'],
+  ['tp1d', 'dut1', 'td2', 'DUT1 TPD', 'tp1d-remind'],
+  ['tp1e', 'dut1', 'td3', 'DUT1 TPE', 'tp1e-remind'],
+  ['tp2a', 'dut2', 'td1', 'DUT2 TPA', 'tp2a-remind'],
+  ['tp2b', 'dut2', 'td1', 'DUT2 TPB', 'tp2b-remind'],
+  ['tp2c', 'dut2', 'td2', 'DUT2 TPC', 'tp2c-remind'],
+  ['tp2d', 'dut2', 'td2', 'DUT2 TPD', 'tp2d-remind'],
 ].map(x => ({
   name: x[0].toLowerCase(),
   yearGroup: x[1].toLowerCase(),
-  tdGroup: x[2].toLowerCase()
+  tdGroup: x[2].toLowerCase(),
+  planningGroup: x[3],
+  remindChannel: x[4]
 }))
 
 export const defaultTpGroupsName = defaultTpGroups.map(x => x.name)
--- src/reminderService.ts
+++ src/reminderService.ts
@@ -1,9 +1,40 @@
+import { TextChannel } from 'discord.js'
 import { CronJob } from 'cron'
-import { AUTO_REMINDER_CRON_TIME, SERVER_ID, logger } from './config'
+
+import { AUTO_REMINDER_CRON_TIME, SERVER_ID, PLANNING_LINK, logger } from './config'
 import { bot } from './bot'
 import msgId from './msgId'
-import { TpGroupModel } from './database/TpGroup'
-import { TextChannel } from 'discord.js'
+import planningIut from './commands/utils/planningIut'
+import { TpGroupModel, TpGroupDocument } from './database/TpGroup'
+
+/**
+ * Remind a TP group of its homework
+ * @param channel TP group remind channel
+ * @param tpGroup TP group to remind
+ */
+const homeworkRemind = (channel: TextChannel, tpGroup: TpGroupDocument) => {
+  // Keep only homework that are due in the future
+  const dateMin = new Date(Date.now() + -1 * 24 * 3600 * 1000)
+  const homeworks = tpGroup.homework ? tpGroup.homework.filter(aHomework => dateMin < aHomework.dueDate) : []
+
+  // Build the message that will be sent
+  return channel.send(`Rappel journalier des devoirs pour le groupe ${tpGroup.name} :\n${homeworks.length === 0
+    ? msgId.NO_HOMEWORK(tpGroup.name)
+    : homeworks.map(aHomework => msgId.HOMEWORK_SHOW(aHomework)).join('\n')}`)
+}
+
+/**
+ * Remind a TP group of its homework
+ * @param channel TP group remind channel
+ * @param tpGroup TP group to remind
+ */
+const planningRemind = async (channel: TextChannel, tpGroup: TpGroupDocument) => {
+  if (!tpGroup.planningGroup) return
+  const planningData = (await planningIut.getGroup(tpGroup.planningGroup))[0]
+  return channel.send(`Rappel journalier du planning pour le groupe ${tpGroup.name} :\n${msgId.PLANNING_SHOW(tpGroup.name, new Date(planningData.screenDate))}`, {
+    file: `${PLANNING_LINK}${planningData.screenPath}`
+  })
+}
 
 /**
  * Starts the reminder service everyday at 19:00
@@ -19,27 +50,21 @@ export default () => new CronJob(AUTO_REMINDER_CRON_TIME, async () => {
   const tpGroups = await TpGroupModel.find()
   for (const aTpGroup of tpGroups) {
     try {
-      // Check the TP group channel exists
-      const channel = guild.channels.find(aChannel => aChannel.name === aTpGroup.name)
+      if (!aTpGroup.remindChannel) continue
+
+      // Check the TP group remind channel exists
+      const channel = guild.channels.find(aChannel => aChannel.name === aTpGroup.remindChannel)
       if (!channel || !(channel instanceof TextChannel))
-        throw new Error(msgId.UNKNOWN_CHANNEL(aTpGroup.name))
+        throw new Error(msgId.UNKNOWN_CHANNEL(aTpGroup.remindChannel))
 
-      // Keep only homework that are due in the future
-      const dateMin = new Date(Date.now() + -1 * 24 * 3600 * 1000)
-      const homeworks = aTpGroup.homework
-        ? aTpGroup.homework.filter(aHomework => dateMin < aHomework.dueDate)
-        : []
-
-      // Build the message that will be sent
-      let msg = `Rappel journalier des devoirs pour le groupe ${aTpGroup.name} :\n\n`
-      if (homeworks.length === 0) msg += msgId.NO_HOMEWORK(aTpGroup.name)
-      else msg += homeworks.map(aHomework => msgId.HOMEWORK_SHOW(aHomework)).join('\n')
-
-      await channel.send(msg)
+      // Remind of the planning
+      await planningRemind(channel, aTpGroup)
+      // Remind of the homework
+      await homeworkRemind(channel, aTpGroup)
     }
     catch (error) {
       logger.error(error)
     }
   }
   logger.info(`Reminder process ended`)
-}, undefined, true, 'Europe/Paris', undefined, false)
+}, undefined, true, 'Europe/Paris', undefined, true)
