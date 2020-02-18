b32185da48578de8bd0d9c2987d07b6b42eabbf7
rigwild <me@rigwild.dev>
2020-02-18 18:50:51 +0100

    Full dependencies upgrade

--- packages/core/src/database/index.ts
+++ packages/core/src/database/index.ts
@@ -7,7 +7,7 @@ export { TpGroupModel } from './TpGroup'
 
 export const connectDb = async () => {
   // Connect to the database
-  await mongoose.connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+  await mongoose.connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true })
   mongoose.connection.on('error', err => {
     throw err
   })
